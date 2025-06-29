7ec58f375dadf280f0a388819f0e307ade566f8a
Maty910 <matychacong@gmail.com>
2025-06-29 20:51:03 -0300

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@ export default {
     slug: "mi-app-de-juegos",
     version: "1.0.0",
     extra: {
-      apiKey: process.env.API_KEY,
+      apiKey: process.env.EXPO_PUBLIC_API_KEY,
     },
   },
 };
